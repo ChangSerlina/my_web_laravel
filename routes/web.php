6afedcb429895a1e07186d82d36595b56f93e911
榕軒 <serlina0504@gmail.com>
2025-08-02 10:49:59 +0800

    [#26] 調整 MVC 架構

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ use App\Http\Controllers\ParkingFeeController;
 use App\Http\Controllers\ParkingFeeCheckController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MailController;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;    // Socialite for OAuth
-use Google\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,66 +19,17 @@ use Google\Client;
 |
 */
 
-// Google OAuth 登入
-Route::get('/auth/google', function () {
-    return Socialite::driver('google')
-    ->scopes(['openid', 'profile', 'email']) // 確保包含 openid
-    ->redirect();
-})->name('auth.google'); // ⬅️ 給路由命名
-
-Route::get('/auth/google/callback', function () {
-    $googleUser = Socialite::driver('google')->user();
-
-    // 檢查 email 是否已經存在於系統中
-    $user = \App\Models\User::where('email', $googleUser->getEmail())->first();
-
-    if(!$user) {
-        $user = \App\Models\User::create([
-            'email' => $googleUser->getEmail(),
-            'name' => $googleUser->getName(),
-            // 'avatar' => $googleUser->getAvatar(), // 可選，紀錄 Google 頭像
-            'google_id' => $googleUser->getId(),    // 儲存 Google ID
-        ]);
-    }else {
-        // 如果已經存在，更新個人資料
-        \App\Models\User::where('email', $googleUser->getEmail())->update([
-            'name' => $googleUser->getName(),
-            // 'avatar' => $googleUser->getAvatar(), // 可選，紀錄 Google 頭像
-            'google_id' => $googleUser->getId(),    // 儲存 Google ID
-        ]);
-    }
-
-    Auth::login($user);
-
-    if (!$user->canAccessPanel(app(\Filament\Panel::class))) {
-        Auth::logout();
-        return redirect('/admin/login')->withErrors(['auth' => '很抱歉，您沒有權限進入後台!請與管理人員聯絡']);
-    }else {
-        // 如果有權限，導向到後台
-        return redirect('/admin');
-    }
-});
+// 後台.Google OAuth 登入
+Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth.google'); // ⬅️ 給路由命名
+Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
 
 // 發送測試信
 // Route::get('/sendTestMail', [MailController::class, 'send']);
 
-// 電子郵件驗證
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/admin');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-// 重新發送驗證郵件
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', '已重新發送驗證信!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+// 前台.電子郵件驗證
+Route::get('/email/verify', [MailController::class, 'notice'])->middleware('auth')->name('verification.notice');
+Route::get('/email/verify/{id}/{hash}', [MailController::class, 'verify'])->middleware(['auth', 'signed'])->name('verification.verify');
+Route::post('/email/verification-notification', [MailController::class, 'resend'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 // 登入
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
@@ -97,7 +44,7 @@ Route::post('/contact/store', [ContactController::class, 'reporting'])->name('re
 Route::get('/parkingFee', [ParkingFeeController::class, 'parkingFee_show'])->name('parkingFee_show');
 Route::get('/captchaImage', [ParkingFeeController::class, 'create_captcha'])->name('create_captcha');
 Route::post('/parkingFee', [ParkingFeeController::class, 'check_captcha'])->name('check_captcha');
-
+// 停車費查詢結果
 Route::post('/parkingFeeCheck', [ParkingFeeCheckController::class, 'parkingFeeCheck_show'])->name('parkingFeeCheck_show');
 
 // 首頁
