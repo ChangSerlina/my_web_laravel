e85f25c0e116d1291ff4ec9aaa4c5cd1cd4bbd42
榕軒 <serlina0504@gmail.com>
2025-08-25 20:43:38 +0800

    [#3] 前台登入/登出

--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -3,6 +3,14 @@
                     <li class="tabs__item item"><a href="{{ route('home_show') }}">首頁</a></li>
                     <li class="tabs__item item"><a href="{{ route('parkingFee_show') }}">停車費查詢</a></li>
                     <li class="tabs__item item"><a href="{{ route('report_show') }}">聯絡我們</a></li>
+                    @guest
+                    <li class="tabs__item item"><a href="{{ route('login') }}">登入</a></li>
+                    @else
+                    <li class="tabs__item item"><a href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{Auth::user()->name}} 登出</a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    @endguest
                 </ul>
             </header>
-            
